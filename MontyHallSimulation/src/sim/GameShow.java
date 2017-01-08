b1a83a00124732c5212c2d0b9f235dd4dc46a34c
Jens Barthel <jens.barthel@gmail.com>
2017-01-08 19:19:32 +0100

    remove unused variable and fix typos in GameShow

--- MontyHallSimulation/src/sim/GameShow.java
+++ MontyHallSimulation/src/sim/GameShow.java
@@ -5,15 +5,15 @@ import java.util.concurrent.ThreadLocalRandom;
 public class GameShow {
 	private int prizePosition;
 	private int leftDudPosition;
-	private int rightDudPositions;
+	private int rightDudPosition;
 	private GameHost gameHost;
 	private Contestant contestant;
 
 	public GameShow() {
 		prizePosition = ThreadLocalRandom.current().nextInt(0, 3);
 		leftDudPosition = Math.floorMod(prizePosition - 1, 3);
-		rightDudPositions = Math.floorMod(prizePosition + 1, 3);
-		gameHost = new GameHost(leftDudPosition, prizePosition, rightDudPositions);
+		rightDudPosition = Math.floorMod(prizePosition + 1, 3);
+		gameHost = new GameHost(leftDudPosition, prizePosition, rightDudPosition);
 		contestant = new Contestant();
 	}
 
@@ -21,13 +21,11 @@ public class GameShow {
 		int contestantDecision = contestant.announceDecision();
 		int alternativePosition = gameHost.proposeAlternative(contestantDecision);
 		contestant.decideOnSwitch(alternativePosition);
-		contestantDecision = contestant.announceDecision();
 		return resolve();
 	}
 
 	private ShowResult resolve() {
 		int contestantFinalDecision = contestant.announceDecision();
-
 		boolean hasWon = contestantFinalDecision == prizePosition ? true : false;
 		boolean hasSwitched = contestant.hasSwiched();
 		return new ShowResult(hasSwitched, hasWon);
